670f45d797f56272fdbfb6a31883c543f21b3df7
Danny Rangel <danielrangel.tech@gmail.com>
2019-11-29 23:44:33 -0800

    remove test route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,6 @@ const passport = require('passport');
 
 // Google OAuth Stuff
 module.exports = app => {
-    app.get('/hi', (req, res) => {
-        res.send('hi');
-    });
     app.get(
         '/auth/google',
         passport.authenticate('google', {
